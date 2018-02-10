bba8727b284c776339e83eaa6850baa0799bee71
Mike Barajas <mikebarajas@live.com>
2018-02-10 09:39:08 -0600

    pushing early pagination build

--- Debug/pages.js
+++ Debug/pages.js
@@ -1,44 +1,3 @@
-// Get references to the tbody element, input field and button
-var $tbody = document.querySelector("tbody");
-var $dateSearch = document.querySelector("#datetime");
-var $citySearch = document.querySelector("#city");
-var $stateSearch = document.querySelector("#state");
-var $countrySearch = document.querySelector("#country");
-var $shapeSearch = document.querySelector("#shape");
-var $searchBtn = document.querySelector("#search");
-
-
-
-
-
-// Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-$searchBtn.addEventListener("click", handleSearchButtonClick);
-
-// Set filteredAddresses to data initially
-var filteredUFO = ufoData1;
-
-// renderTable renders the filteredAddresses to the tbody
-function renderTable() {
-  $tbody.innerHTML = "";
-  for (var i = 0; i < filteredUFO.length; i++) {
-    // Get get the current address object and its fields
-    var UFO = filteredUFO[i];
-    var fields = Object.keys(UFO);
-    // Create a new row in the tbody, set the index to be i + startingIndex
-    var $row = $tbody.insertRow(i);
-    for (var j = 0; j < fields.length; j++) {
-      // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
-      var field = fields[j];
-      var $cell = $row.insertCell(j);
-      $cell.innerText = UFO[field];
-    }
-  }
-}
-
-
-// Render the table for the first time on page load
-renderTable();
-
 
 // get the table element
 var $table = document.getElementById("myTable"),
@@ -103,33 +62,4 @@ function pageButtons($pCount,$cur) {
 		$buttons += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
 	$buttons += "<input type='button' value='Next &gt;&gt;' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
 	return $buttons;
-}
-
-function handleSearchButtonClick() {
-  console.log("click")
-  filteredUFO = ufoData1
-  // create a list of searches
-  var searches ={
-    dateTime: $dateSearch.value, 
-    city: $citySearch.value.trim().toLowerCase(), 
-    state: $stateSearch.value.trim().toLowerCase(), 
-    country: $countrySearch.value.trim().toLowerCase(), 
-    shape: $shapeSearch.value.trim().toLowerCase()
-  }
-  console.log(searches)
-  // filter through keys and values
-  var searchParameter = Object.keys(searches)
-  var searchFilter = Object.values(searches)
-  filteredUFO = filteredUFO.filter(x => {
-    for (i = 0; i < 5; i++) {
-      if (searchFilter[i] !=="") {
-          if (x[searchParameter[i]] !== searchFilter[i]){
-            return false  
-          }
-      }
-    } 
-    return true 
-    renderTable();
-    console.log("click2")
-  })
 }
